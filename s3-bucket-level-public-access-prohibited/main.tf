9a2c554dbe5468a1f933561e5b35997ddf3e97fe
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-04-05 22:14:14 +0800

    Update main.tf - try this

--- s3-bucket-level-public-access-prohibited/main.tf
+++ s3-bucket-level-public-access-prohibited/main.tf
@@ -1,83 +1,55 @@
+# main.tf
+
+# Configure the AWS provider
 provider "aws" {
   region = var.region
 }
 
 variable "region" {
-  default = "us-east-1"
+  default = "us-east-1" # Change to your preferred region
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
+resource "aws_iam_role" "config_lambda_role" {
+  name = "config_lambda_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_policy" "lambda_s3_policy" {
-  name        = "lambda_s3_policy"
-  description = "Policy for S3 access required by Lambda function"
-  
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.config_lambda_role.id
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "s3:ListBuckets",
-          "s3:ListObjectsV2",
-          "s3:GetBucketPolicy",
-          "s3:GetBucketAcl",
-          "s3:GetPublicAccessBlock"
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetBucketPublicAccessBlock",
+          "config:PutEvaluations"
         ]
         Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
       }
     ]
   })
-}
-
-resource "aws_iam_policy_attachment" "lambda_basic_execution" {
-  name       = "lambda_basic_execution"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_policy_attachment" "config_rules_execution" {
-  name       = "config_rules_execution"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRulesExecutionRole"
-}
-
-resource "aws_iam_policy_attachment" "lambda_s3_policy_attachment" {
-  name       = "lambda_s3_policy_attachment"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = aws_iam_policy.lambda_s3_policy.arn
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
-  principal     = "events.amazonaws.com"
-}
-
-resource "null_resource" "package_lambda" {
-  provisioner "local-exec" {
-    command = <<EOT
-      zip -r lambda_function.zip lambda_function.py
-    EOT
-    working_dir = "${path.module}"
-  }
 }
 
 data "archive_file" "lambda_zip" {
